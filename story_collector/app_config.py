f8dad69542a30166640f88292e15723da09c15f2
lamthuyvo <lamthuyvo@gmail.com>
2016-12-05 14:13:39 -0500

    committing to pull

--- story_collector/app_config.py
+++ story_collector/app_config.py
@@ -8,4 +8,4 @@ DB_NAME = os.getenv('MYSQL_DB', 'story_collector')
 ADMIN_USER = os.getenv('ADMIN_USER', 'admin')
 ADMIN_PASS = os.getenv('ADMIN_PASS', 'something-secret')
 
-USE_FAKE_DATA = os.getenv('USE_FAKE_DATA', False) # set this to True to use dummy data instead of db
+USE_FAKE_DATA = os.getenv('USE_FAKE_DATA', True) # set this to True to use dummy data instead of db
